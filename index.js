e4e857d74643a9f0cca49561ea36e44bc2994c2b
unknown <rahulkumar32034@gmail.com>
2021-07-06 10:07:46 +0530

    update1

--- index.js
+++ index.js
@@ -10,28 +10,37 @@ app.use('/', express.static('/'));
 app.use('/images', express.static('images'));
 
 
-app.get('/', async(req, res) => {
-  fs.readdir(
-  path.resolve(__dirname, 'C:\\'),
-  (err, files) => {
-    res.status(200);
-    files.forEach(function (data) {
-      console.log(data);
-      if(data.includes(".txt")){
+app.get('/', async (req, res) => {
+  
+  res.status(200);
+  const directoryPath = path.join(__dirname, "/custom");
+  
+  fs.readdir(directoryPath, function (err, files) {
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      }
+      
+      files.forEach(function (data) {
           console.log(data);
-          res.write('<div style="display:flex; align-items:center;"><img src ="./images/sys.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
-      }
-      else if(data.includes('.sys')){
-          res.write('<div style="display:flex; align-items:center;"><img src ="./images/sys.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
-      }
-      else{
-          res.write('<div style="display:flex; align-items:center;"><img src ="./images/folderIcon.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
-      }
+          if(data.includes(".txt")){
+              console.log(data);
+              res.write('<div style="display:flex; align-items:center;"><img src ="./images/sys.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
+          }
+          else if(data.includes('.sys')){
+              res.write('<div style="display:flex; align-items:center;"><img src ="./images/sys.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
+          }
+          else if(data.includes('.html')){
+              res.write('<div style="display:flex; align-items:center;"><img src ="./images/html.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
+          }
+          else if(data.includes('.js')){
+              res.write('<div style="display:flex; align-items:center;"><img src ="./images/js.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
+          }
+          else{
+              res.write('<div style="display:flex; align-items:center;"><img src ="./images/folderIcon.png" width="20px"/><p style="padding-left:5px">'+data+'</p></div>');
+          }
+      });
+    });
   });
-});
-})
-
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
